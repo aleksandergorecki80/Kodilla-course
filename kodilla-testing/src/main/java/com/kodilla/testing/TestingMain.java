d5be95c381f47d496ee28f88873cfafe128085a3
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2024-09-14 15:47:45 +0200

    6.4. Pisanie testów za pomocą JUnit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,17 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.forum.ForumPost;
+
 public class TestingMain {
 
   public static void main(String[] args) {
+
+
+    ForumPost forumPost = new ForumPost("Body", "Author");
+    ForumPost forumPost2 = new ForumPost("Body", "Author");
+
+    System.out.println(forumPost.getClass());
+    System.out.println(ForumPost.class);
 
   }
 }
