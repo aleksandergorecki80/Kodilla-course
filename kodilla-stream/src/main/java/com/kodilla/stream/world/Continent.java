6c94d85e94200abb67529f6215bbc6023e2f5dc1
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2024-09-22 13:45:51 +0200

    7.4 - Zadanie: Obliczamy liczbę ludzi na świec

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
@@ -0,0 +1,23 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Continent {
+  private final String continentName;
+  private final List<Country> countries;
+
+  public Continent(final String continentName, final List<Country> countries) {
+    this.continentName = continentName;
+    this.countries = countries;
+  }
+
+  public String getContinentName() {
+    return continentName;
+  }
+
+  public List<Country> getCountries() {
+    return countries;
+  }
+}
