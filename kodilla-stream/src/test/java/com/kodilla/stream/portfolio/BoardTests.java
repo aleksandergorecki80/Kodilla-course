2d0b61d29526369077908163c43b29a79d2d95ae
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2024-09-26 17:20:10 +0200

    Zadanie: średnia liczba dni realizacji zadania

--- kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTests.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 
 import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -142,5 +144,32 @@ public class BoardTests {
         .count();
     // Then
     assertEquals(2, longTasks);
+  }
+
+  @Test
+  void testAddTaskListAverageWorkingOnTask() {
+    Board project = prepareTestData();
+
+    // When
+    List<TaskList> inProgressTasks = new ArrayList<>();
+    inProgressTasks.add(new TaskList("In progress"));
+
+    double sumOfDays = project.getTaskLists().stream()
+        .filter(inProgressTasks::contains)
+        .flatMap(taskList -> taskList.getTasks().stream())
+        .map(Task::getCreated)
+        .map(createdDate -> Period.between(createdDate, LocalDate.now()))
+        .map(period -> period.getYears() * 365 + period.getMonths() * 30 + period.getDays())
+        .reduce(0, (a, b) -> a + b);
+
+
+    long numberOfOrders = project.getTaskLists().stream()
+        .filter(inProgressTasks::contains)
+        .flatMap(taskList -> taskList.getTasks().stream())
+        .map(Task::getCreated)
+        .filter(createdDate -> !Period.between(createdDate, LocalDate.now()).isNegative())
+        .count();
+
+    assertEquals(10, sumOfDays/numberOfOrders);
   }
 }
