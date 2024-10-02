95570f0a72cbbe8ab0bf41e87efc509f23f7cce4
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2024-10-02 19:57:52 +0200

    10.1. Inversion of Control i Dependency Injection

--- kodilla-good-patterns/src/main/java/com/kodilla/patterns/carrental/InformationService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/patterns/carrental/InformationService.java
@@ -1,5 +1,5 @@
-package com.kodilla.patterns.carrental;
-
-public interface InformationService {
-  inform();
-}
+//package com.kodilla.patterns.carrental;
+//
+//public interface InformationService {
+//  inform();
+//}
