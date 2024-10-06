fbb2f97da3e1f4a2a2cbccb2ccd39f0d2f5d6916
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2024-10-06 07:58:13 +0200

    10.3. Konfiguracja przy pomocy klasy Java

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -1,26 +1,23 @@
 package com.kodilla.spring.library;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
 public class Library {
   private final List<String> books = new ArrayList<>();
 
-  @Autowired
+//  @Autowired
   private LibraryDbController libraryDbController;
 
 //  @Autowired
-//  public Library(final LibraryDbController libraryDbController) {
-//    this.libraryDbController = libraryDbController;
-//  }
-//
-//  public Library () {
-//    // do nothing
-//  }
+  public Library(final LibraryDbController libraryDbController) {
+    this.libraryDbController = libraryDbController;
+  }
+
+  public Library () {
+    // do nothing
+  }
 
 //  @Autowired
 //  public void setLibraryDbController(LibraryDbController libraryDbController) {
