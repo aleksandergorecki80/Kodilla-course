522853b5cb54c9541cd10f3bda02dedd4bc2a02d
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2024-10-26 18:30:26 +0200

    13.3 - Tworzymy relacje pomiedzy encjami

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Employee.java
@@ -0,0 +1,72 @@
+package com.kodilla.hibernate.manytomany;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "EMPLOYEES")
+public class Employee {
+  private int id;
+  private String firstName;
+  private String lastName;
+  private List<Company> companies = new ArrayList<>();
+
+  public Employee() {
+  }
+
+  public Employee(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  @Id
+  @GeneratedValue
+  @NotNull
+  @Column(name = "EMPLOYEE_ID")
+  public int getId() {
+    return id;
+  }
+
+  @NotNull
+  @Column(name = "FIRST_NAME")
+  public String getFirstName() {
+    return firstName;
+  }
+
+  @NotNull
+  @Column(name = "LAST_NAME")
+  public String getLastName() {
+    return lastName;
+  }
+
+  private void setId(final int id) {
+    this.id = id;
+  }
+
+  private void setFirstName(final String firstName) {
+    this.firstName = firstName;
+  }
+
+
+  private void setLastName(final String lastName) {
+    this.lastName = lastName;
+  }
+
+  @ManyToMany(cascade = CascadeType.ALL)
+  @JoinTable(
+      name = "JOIN_COMPANY_EMPLOYEE",
+      joinColumns = {@JoinColumn(name = "EMPLOYEE_ID", referencedColumnName = "EMPLOYEE_ID")},
+      inverseJoinColumns = {@JoinColumn(name = "COMPANY_ID", referencedColumnName = "COMPANY_ID")}
+  )
+  public List<Company> getCompanies() {
+    return companies;
+  }
+
+  public void setCompanies(List<Company> companies) {
+    this.companies = companies;
+  }
+}
--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -1,5 +1,6 @@
 package com.kodilla.hibernate.task;
 
+import com.kodilla.hibernate.tasklist.TaskList;
 import jakarta.persistence.*;
 import org.antlr.v4.runtime.misc.NotNull;
 
@@ -10,16 +11,18 @@ import java.util.Date;
 public class Task {
   private int id;
   private String description;
-  private int duration;
   private Date created;
+  private int duration;
+  private TaskFinancialDetails taskFinancialDetails;
+  private TaskList taskList;
 
   public Task() {
   }
 
   public Task(String description, int duration) {
     this.description = description;
-    this.duration = duration;
     this.created = new Date();
+    this.duration = duration;
   }
 
   @Id
@@ -60,5 +63,25 @@ public class Task {
 
   private void setCreated(final Date created) {
     this.created = created;
+  }
+
+  @OneToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+  @JoinColumn(name = "TASKS_FINANCIALS_ID")
+  public TaskFinancialDetails getTaskFinancialDetails() {
+    return taskFinancialDetails;
+  }
+
+  public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
+    this.taskFinancialDetails = taskFinancialDetails;
+  }
+
+  @ManyToOne
+  @JoinColumn(name = "TASKLIST_ID")
+  public TaskList getTaskList() {
+    return taskList;
+  }
+
+  public void setTaskList(TaskList taskList) {
+    this.taskList = taskList;
   }
 }
