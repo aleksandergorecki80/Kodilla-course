9f6871423829125b423913efa0b870a65132096d
Aleksander <aleksandergorecki80@gmail.com>
2025-01-01 22:48:34 +0000

    20.2 - Wzorzec Dekorator - add interface and first test

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/dekorator/taxiportal/TaxiOrderTests.java
@@ -0,0 +1,32 @@
+package com.kodilla.patterns2.dekorator.taxiportal;
+
+import com.kodilla.patterns2.decorator.taxiportal.BasicTaxiOrder;
+import com.kodilla.patterns2.decorator.taxiportal.TaxiOrder;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TaxiOrderTests {
+
+    @Test
+    public void testBasicTaxiOrderGetCost() {
+        // Given
+        TaxiOrder theOrder = new BasicTaxiOrder();
+        // When
+        BigDecimal calculatedCost = theOrder.getCost();
+        // Then
+        assertEquals(new BigDecimal(5), calculatedCost);
+    }
+
+    @Test
+    public void testBasicTaxiOrderGetDescription() {
+        // Given
+        TaxiOrder theOrder = new BasicTaxiOrder();
+        // When
+        String description = theOrder.getDescription();
+        // Then
+        assertEquals("Drive a course", description);
+    }
+}
