186e6f1f7929a58ffe60a8e34abea1afcbbe9658
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2025-01-01 12:46:29 +0000

    12.6 - Zadanie: aktywni użytkownicy (#24)
    * 12.6 - Zadanie: aktywni użytkownicy
    
    * 13.1 - Zadanie: Dołączamy framework Hibernate (#25)

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTests.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTests.java
@@ -70,4 +70,34 @@ class DbManagerTests {
     assertEquals(5, counter);
   }
 
+  @Test
+  void testSelectUsersAndPosts() throws SQLException {
+    // Given
+    DbManager dbManager = DbManager.getInstance();
+
+    //When
+    String sqlQuery = """
+           SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS_NUMBER
+           FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID
+           GROUP BY P.USER_ID
+           HAVING COUNT(*) >= 2
+        """;
+
+    Statement statement = dbManager.getConnection().createStatement();
+    ResultSet rs = statement.executeQuery(sqlQuery);
+
+    // Then
+    int counter = 0;
+    while (rs.next()) {
+      System.out.println(
+          rs.getString("FIRSTNAME") + " " +
+          rs.getString("LASTNAME") + " - " +
+          rs.getString("POSTS_NUMBER") + " posts"
+          );
+      counter++;
+    }
+    rs.close();
+    statement.close();
+    assertEquals(2, counter);
+  }
 }
