515556b9ed1952bf804fe48c8962ee07ca2f9dd2
Aleksander <aleksandergorecki80@gmail.com>
2025-01-02 09:58:57 +0000

    20.2 - Zadanie: Dekorujemy pizzę - add basic pizza order and test

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/decorator/pizza/AbstractPizzaOrderDecorator.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/decorator/pizza/AbstractPizzaOrderDecorator.java
@@ -1,4 +1,21 @@
 package com.kodilla.patterns2.decorator.pizza;
 
-public class AbstractPizzaOrderDecorator {
+import java.math.BigDecimal;
+
+public class AbstractPizzaOrderDecorator implements PizzaOrder{
+    private final PizzaOrder pizzaOrder;
+
+    public AbstractPizzaOrderDecorator(PizzaOrder pizzaOrder) {
+        this.pizzaOrder = pizzaOrder;
+    }
+
+    @Override
+    public BigDecimal getCost() {
+        return this.pizzaOrder.getCost();
+    }
+
+    @Override
+    public String getIngredients() {
+        return this.pizzaOrder.getIngredients();
+    }
 }
--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/dekorator/pizza/PizzaOrderTests.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/dekorator/pizza/PizzaOrderTests.java
@@ -1,4 +1,36 @@
 package com.kodilla.patterns2.dekorator.pizza;
 
+import com.kodilla.patterns2.decorator.pizza.BasicPizzaOrder;
+import com.kodilla.patterns2.decorator.pizza.PizzaOrder;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PizzaOrderTests {
+
+    @Test
+    public void testBasicPizzaOrderCost() {
+        // Given
+        PizzaOrder pizzaOrder = new BasicPizzaOrder();
+
+        // When
+        BigDecimal cost = pizzaOrder.getCost();
+
+        // Then
+        assertEquals(new BigDecimal(15), cost);
+    }
+
+    @Test
+    public void testBasicPizzaOrderIngredients() {
+        // Given
+        PizzaOrder pizzaOrder = new BasicPizzaOrder();
+
+        // When
+        String ingredients = pizzaOrder.getIngredients();
+
+        // Then
+        assertEquals("Ingredients: base, tomato sauce, cheese", ingredients);
+    }
 }
