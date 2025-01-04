81cadd47da7de043a2f705b0f02ecc9363c4020e
Aleksander <aleksandergorecki80@gmail.com>
2025-01-04 09:15:28 +0000

    20.3. Programowanie aspektowe w Spring Boot - Zadanie

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacadeWatcher.java
@@ -0,0 +1,29 @@
+package com.kodilla.patterns2.facade.api;
+
+import com.kodilla.patterns2.aop.calculator.Watcher;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Aspect
+@Component
+public class OrderFacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacadeWatcher.class);
+
+    @AfterThrowing(value = "execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))", throwing = "exception")
+    public void logException(OrderProcessingException exception) {
+        LOGGER.error("Exception occurred: " + exception.getMessage());
+    }
+
+    @Before(value = "execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..)) && args(order, userId) && target(object)",
+            argNames = "order,userId,object")
+    public void logProcessedOrder(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + "User ID: " + userId + " Order details: " + order);
+    }
+
+}
