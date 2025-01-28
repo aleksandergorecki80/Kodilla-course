2e921fc9d187c15d78f4d1be363fbf39c01b2762
Aleksander <aleksandergorecki80@gmail.com>
2025-01-28 19:16:42 +0100

    add webdrivermanager

--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -1,12 +1,12 @@
 package com.kodilla.testing2.google;
 
-import com.kodilla.testing2.config.WebDriverConfig;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 
 public class GoogleTestingApp {
 
     public static void main(String[] args) {
-        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        WebDriver driver = WebDriverManager.firefoxdriver().create();
         driver.get("https://www.google.com");
     }
 }
