46207944ce3f34f9b36249355823d187c2c30c0c
Aleksander <aleksandergorecki80@gmail.com>
2025-02-01 13:24:00 +0100

    23. Rozszerzenie testów i testy integracyjne

--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -1,12 +1,22 @@
 package com.kodilla.testing2.google;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 
 public class GoogleTestingApp {
+    public static final String SEARCHFIELD = "q";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverManager.firefoxdriver().create();
         driver.get("https://www.google.com");
+
+        driver.findElement(By.xpath("//*[@id=\"L2AGLb\"]/div")).click();
+
+        WebElement searchField = driver.findElement(By.name(SEARCHFIELD));
+        searchField.sendKeys("Kodilla");
+        searchField.submit();
     }
 }
