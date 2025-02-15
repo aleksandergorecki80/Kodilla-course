05195cf0aa2d2862a0525dced4532a801b37b443
Aleksander <aleksandergorecki80@gmail.com>
2025-02-15 18:34:01 +0100

    23.3. Tworzenie testów dla aplikacji Tasks - Zadanie: Sprzątamy w aplikacji CRUD App.

--- kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
+++ kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
@@ -11,17 +11,17 @@ import org.openqa.selenium.support.ui.Select;
 
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CrudAppTestSuite {
-    private static final String BASE_URL = "http://localhost/crud/";
+    private static final String BASE_LOCAL_URL = "http://localhost/crud/";
+//    private static final String BASE_REMOTE_URL = "https://aleksandergorecki80.github.io/";
     private WebDriver driver;
     private Random generator;
 
     @BeforeEach
     public void initTests() {
         driver = WebDriverManager.chromedriver().create();
-        driver.get(BASE_URL);
+        driver.get(BASE_LOCAL_URL);
         generator = new Random();
     }
 
@@ -72,9 +72,28 @@ public class CrudAppTestSuite {
         Thread.sleep(5000);
     }
 
+    private void deleteCrudAppTestTask(String taskName) throws InterruptedException {
+        driver.navigate().refresh();
+
+        while(!driver.findElement(By.xpath("//select[1]")).isDisplayed());
+
+        driver.findElements(
+                        By.xpath("//form[@class=\"datatable__row\"]")).stream()
+                .filter(anyForm ->
+                        anyForm.findElement(By.xpath(".//p[@class=\"datatable__field-value\"]"))
+                                .getText().equals(taskName))
+                .forEach(theForm -> {
+                    WebElement deleteButton = theForm.findElement(By.xpath(".//button[@data-task-delete-button]"));
+
+                    deleteButton.click();
+                });
+        Thread.sleep(5000);
+    }
+
     @Test
     public void shouldCreateTrelloCard() throws InterruptedException {
         String taskName = createCrudAppTestTask();
         sendTestTaskToTrello(taskName);
+        deleteCrudAppTestTask(taskName);
     }
 }
